90ff02ec66de2cff85440c088b5551bf478e3eeb
LaoBing_Fung <FLB_B401@163.com>
2018-12-28 10:36:07 +0800

    add best-charge and test

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,19 @@
+'use strict';
+let getItemsList = require('./get_items_list.js');
+let computeItemsSubtotalList = require('./compute_items_subtotal_list.js');
+let computeDiscount6Per30 = require('./compute_discount_6_per_30.js');
+let computeDiscountHalf = require('./compute_discount_half.js');
+let computeDiscount = require('./compute_discount.js');
+let computeTotalPrice = require('./compute_total_price.js');
+let getBill = require('./get_bill.js');
+let loadAllItems = require('./items.js');
+let loadPromotions = require('./promotions.js');
+
 function bestCharge(selectedItems) {
-  return /*TODO*/;
+  let itemsList = getItemsList(selectedItems);
+  let itemsPriceList = computeItemsSubtotalList(itemsList);
+  let promotion = computeDiscount(itemsPriceList);
+  let totalPrice = computeTotalPrice(itemsPriceList, promotion);
+  return getBill(itemsPriceList, promotion, totalPrice);
 }
+// module.exports = bestCharge;
